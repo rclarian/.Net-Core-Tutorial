d8c2ef7bf1131f02d46de37af66c0913d75117e9
Ryan Clarian <rclarian@gmail.com>
2024-07-03 20:45:55 +0800

    #10 HTTP status codes | HTTP Request & Response
    #10 HTTP status codes | HTTP Request & Response

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -6,13 +6,35 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //Creating a route - HTTP method + URL
-app.MapGet("/", (HttpContext context) => 
-    {
+//app.MapGet("/", (HttpContext context) => 
+//    {
+//        context.Response.Headers["Content-Type"] = "text/html";
+//        context.Response.Headers["MyHeader"] = "Hello World";
+//        return "<h2>This is a Text response</h2>";
+//    }
+//);
 
-        context.Response.Headers["Content-Type"] = "text/html";
-        return "<h2>This is a Text response</h2>";
+app.Run(async (HttpContext context) =>
+{
+    string path = context.Request.Path;
+
+    if(path == "/" || path == "/Home")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("You are in the Home page");
     }
-);
+    else if(path == "Contact")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("You are in the Contact page");
+    }
+    else
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("Page not found!");
+    }
+    
+});
 
 //Starting the server
 app.Run();
