b50c3fae5337907d8ac0a635ea49a44ef7047244
Ryan Clarian <rclarian@gmail.com>
2024-07-04 18:56:45 +0800

    #17 Chaining Multiple Middleware
    #17 Chaining Multiple Middleware

--- Middleware/Middleware/Program.cs
+++ Middleware/Middleware/Program.cs
@@ -6,10 +6,24 @@ var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
-//middleware
+//Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Welcome from ASP.NET Core App 1!");
+    await next(context);
+});
+
+//Middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("\n\n Welcome from ASP.NET Core App 2!");
+    await next(context);
+});
+
+//Middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Welcome from ASP.NET Core App!");
+    await context.Response.WriteAsync("This is my first ASP.NET Core App 3!");
 });
 
 
