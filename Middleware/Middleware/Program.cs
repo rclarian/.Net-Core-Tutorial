c5d24a6978bc4ded46bf40302dcb0a7517b22edb
Ryan Clarian <rclarian@gmail.com>
2024-07-04 12:45:29 +0800

    #16 Creating a Custom Middleware
    #16 Creating a Custom Middleware

--- Middleware/Middleware/Program.cs
+++ Middleware/Middleware/Program.cs
@@ -1,6 +1,16 @@
+//1. Create an Intance of web application builder
 var builder = WebApplication.CreateBuilder(args);
+
+//2. Create an instance of WebApplication
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//middleware
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Welcome from ASP.NET Core App!");
+});
+
 
 app.Run();
