7278a59813b47afe8548a8bb51b867bb0fd43267
Ryan Clarian <rclarian@gmail.com>
2024-07-05 20:24:30 +0800

    #23 Routing in ASP NET Core -- app.UseRouting();, app.UseEndpoints(
    #23 Routing in ASP NET Core -- app.UseRouting();, app.UseEndpoints(

--- Routing/Routing/Program.cs
+++ Routing/Routing/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseRouting();
+
+app.UseEndpoints(endpoint => 
+{  
+    //Define routes
+    
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
