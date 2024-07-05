3c991e3687e6ae2a502d2f65aca90182d1c9a8a6
Ryan Clarian <rclarian@gmail.com>
2024-07-05 22:02:32 +0800

    #24 Understanding Map(), MapGet() & MapPost()
    #24 Understanding Map(), MapGet() & MapPost()

--- Routing/Routing/Program.cs
+++ Routing/Routing/Program.cs
@@ -4,11 +4,28 @@ var app = builder.Build();
 app.UseRouting();
 
 app.UseEndpoints(endpoint => 
-{  
+{
     //Define routes
-    
+    endpoint.Map("/Home", async (context) =>
+    {
+        await context.Response.WriteAsync("Your are in the Home page");
+    });
+
+    endpoint.MapGet("/Product", async (context) =>
+    {
+        await context.Response.WriteAsync("Your are in the Products page");
+    });
+
+    endpoint.MapPost("/Product", async (context) =>
+    {
+        await context.Response.WriteAsync("A new Product created");
+    });
 });
 
-app.MapGet("/", () => "Hello World!");
+//Short circuit middleware
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("The URL which you are looking for is not found!");
+});
 
 app.Run();
