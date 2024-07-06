8aa705a54a7aaece90f2fba3620716bb2678efc1
Ryan Clarian <rclarian@gmail.com>
2024-07-06 14:54:42 +0800

    #27 Default route parameter value - endpoint.MapGet("/products/{id=101}
    #27 Default route parameter value - endpoint.MapGet("/products/{id=101}

--- Routing/Routing/Program.cs
+++ Routing/Routing/Program.cs
@@ -7,13 +7,13 @@ app.UseRouting();
 
 app.UseEndpoints(endpoint =>
 {
-    endpoint.MapGet("/products/{id}", async (context) =>
+    endpoint.MapGet("/products/{id=101}", async (context) =>
     {
         var id = Convert.ToInt32(context.Request.RouteValues["id"]);
         await context.Response.WriteAsync($"This is product with ID {id}");
     });
 
-    endpoint.MapGet("/books/author/{authorname}/{bookid}", async (context) =>
+    endpoint.MapGet("/books/author/{authorname='John-Smith'}/{bookid=1}", async (context) =>
     {
         var bookId = Convert.ToInt32(context.Request.RouteValues["bookid"]);
         var authorName = Convert.ToString(context.Request.RouteValues["authorname"]);
