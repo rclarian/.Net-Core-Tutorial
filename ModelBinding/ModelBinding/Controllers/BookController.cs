173de3fb4eeade2ee4194a94961947aaf3b4c73e
Ryan Clarian <rclarian@gmail.com>
2024-07-08 07:53:44 +0800

    #44 Query String & Route Parameter Binding - /Books/102/RYAN
    #44 Query String & Route Parameter Binding - /Books/102/RYAN

--- ModelBinding/ModelBinding/Controllers/BookController.cs
+++ ModelBinding/ModelBinding/Controllers/BookController.cs
@@ -5,9 +5,10 @@ namespace ModelBinding.Controllers
 {
     public class BookController : Controller
     {
-        //URL - /Books?bookid=101&author=steve
-        [Route("/Books")]
-        public IActionResult Book(int bookid, string author)
+        //URL query string - /Books?bookid=101&author=steve
+        //URL route parameters - /Books/102/RYAN
+        [Route("/Books/{BookId}/{Author}")]
+        public IActionResult Book(int? bookid, string author)
         {
           
             return Content($"Book ID is: {bookid} \nAuthor is: {author}", "text/plain");
