de84b688601192679b81b5271fa5f0b31df012b0
Ryan Clarian <rclarian@gmail.com>
2024-07-07 15:42:56 +0800

    #39 Understanding File Result - inside file VirtualFileResult
    #39 Understanding File Result - inside file VirtualFileResult

---
+++ ASP.NET.Controllers/ASP.NET.Controllers/Controllers/FileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.Controllers.Controllers
+{
+    public class FileController : Controller
+    {
+        [Route("File/Download-file")]
+        public VirtualFileResult Index()
+        {
+            return File("/Samples/Sample.pdf", "application/pdf");
+        }
+    }
+}
