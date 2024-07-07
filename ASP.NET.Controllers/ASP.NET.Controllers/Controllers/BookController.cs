ed1db80e47c60654900e1e9baaf21d428bf0bb30
Ryan Clarian <rclarian@gmail.com>
2024-07-07 20:12:27 +0800

    #42 Understanding RedirectToAction Result
    #42 Understanding RedirectToAction Result

--- ASP.NET.Controllers/ASP.NET.Controllers/Controllers/BookController.cs
+++ ASP.NET.Controllers/ASP.NET.Controllers/Controllers/BookController.cs
@@ -8,24 +8,29 @@ namespace ASP.NET.Controllers.Controllers
         [Route("/Books")]
         public IActionResult Books()
         {
-            if (!Request.Query.ContainsKey("BookId"))
-            {
-                return BadRequest("Book ID is not Provided!");
-            }
+            //if (!Request.Query.ContainsKey("BookId"))
+            //{
+            //    return BadRequest("Book ID is not Provided!");
+            //}
 
-            int bookId = Convert.ToInt32(Request.Query["BookId"]);
-            if(bookId < 1 || bookId > 1000)
-            {
-                return NotFound("Book ID is not in Range of 1 to 1000");
-            }
+            //int bookId = Convert.ToInt32(Request.Query["BookId"]);
+            //if(bookId < 1 || bookId > 1000)
+            //{
+            //    return NotFound("Book ID is not in Range of 1 to 1000");
+            //}
 
-            if (!Convert.ToBoolean(Request.Query["IsLogged"]))
-            {
-                //return Unauthorized("You are not logged in!");
-                return StatusCode(Microsoft.AspNetCore.Http.StatusCodes.Status401Unauthorized);
-            }
+            //if (!Convert.ToBoolean(Request.Query["IsLogged"]))
+            //{
+            //    //return Unauthorized("You are not logged in!");
+            //    return StatusCode(Microsoft.AspNetCore.Http.StatusCodes.Status401Unauthorized);
+            //}
+            //return File("/Samples/Sample.pdf", "application/pdf");
 
-            return File("/Samples/Sample.pdf", "application/pdf");
+            //return RedirectToAction("Books", "Store", new {});
+            //301 - Moved permanently
+            //302 - Found(Moved temporarily)
+            //return new RedirectToActionResult("Books", "Store", new { }, true);
+            return RedirectToActionPermanent("Books", "Store", new { });
         }
     }
 }
---
+++ ASP.NET.Controllers/ASP.NET.Controllers/Controllers/StoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.Controllers.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("/category/books")]
+        public IActionResult Books()
+        {
+            return Content("Books page", "text/plain");
+        }
+    }
+}
