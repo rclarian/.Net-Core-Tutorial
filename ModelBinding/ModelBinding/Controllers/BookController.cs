37fc2d4f9109f99243f75b9d5e44445f7391c5a8
Ryan Clarian <rclarian@gmail.com>
2024-07-08 07:46:18 +0800

    #44 Query String & Route Parameter Binding - Books?bookid=101&author=steve
    #44 Query String & Route Parameter Binding - Books?bookid=101&author=steve

---
+++ ModelBinding/ModelBinding/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ModelBinding.Controllers
+{
+    public class BookController : Controller
+    {
+        //URL - /Books?bookid=101&author=steve
+        [Route("/Books")]
+        public IActionResult Book(int bookid, string author)
+        {
+          
+            return Content($"Book ID is: {bookid} \nAuthor is: {author}", "text/plain");
+        }
+    }
+}
