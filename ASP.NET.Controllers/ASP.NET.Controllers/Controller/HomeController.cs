12a8818b24eabfc4f68b2bea4c3b1e860ca8e3ac
Ryan Clarian <rclarian@gmail.com>
2024-07-07 12:22:08 +0800

    #36 Multiple action methods - home, about, contact
    #36 Multiple action methods - home, about, contact

--- ASP.NET.Controllers/ASP.NET.Controllers/Controller/HomeController.cs
+++ ASP.NET.Controllers/ASP.NET.Controllers/Controller/HomeController.cs
@@ -2,12 +2,32 @@
 
 namespace ASP.NET.Controllers.Controller
 {
+    //[Controller]
     public class HomeController
     {
         [Route("Home")]
+        [Route("/")]
         public string Index()
         {
             return "Welcome from ASP.NET core application";
+        }
+
+        [Route("About")]
+        public string About()
+        {
+            return "You are in About Page!";
+        }
+
+        [Route("Contact-Us")]
+        public string Contact()
+        {
+            return "You are in About Contact!";
+        }
+
+        [Route("/products/{id:int:min(1000):max(9999)}")]
+        public string Products()
+        {
+            return "You are in Products Page!";
         }
     }
 }
