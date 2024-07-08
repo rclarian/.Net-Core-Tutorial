879d5ecd6ab2af079ecb6809037f3264fc6304d6
Ryan Clarian <rclarian@gmail.com>
2024-07-08 13:47:12 +0800

    #48 Built-in Model Validators - StringLength, RegularExpression, Range

--- ModelBinding/ModelBinding/Model/Book.cs
+++ ModelBinding/ModelBinding/Model/Book.cs
@@ -9,8 +9,13 @@ namespace ModelBinding.Model
 
         [Required(ErrorMessage = "{0} is a required field")]
         [Display(Name = "Book Name")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage ="{0} must have characters between {2} and {1}")]
+        [RegularExpression("^[a-zA-Z ]$", ErrorMessage ="{0} is not valid")]
         public string BookName { get; set; }
 
         public string? Author { get; set; }
+
+        [Range(0, 999.99, ErrorMessage ="{0} must be between {1} and {2}")]
+        public Decimal Price { get; set; }
     }
 }
